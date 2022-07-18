8d126de4abdda119b387fee2a09490df377979d2
Erika <erikaamericano393@gmail.com>
2022-07-17 22:36:51 -0300

    add functionally search

--- client/src/pages/Home/index.js
+++ client/src/pages/Home/index.js
@@ -8,6 +8,8 @@ import Axios from "axios"
 
 export default function Home() {
     const [pollList, setPollList] = useState([])
+    const [filter, setFilter] = useState([])
+    const [searchInput, setSearchInput] = useState('')
 
     const history = useHistory()
 
@@ -43,10 +45,23 @@ export default function Home() {
     }
 
     const voteController = (progressReturn) => {
-        if(progressReturn == "Pendent" || progressReturn == "Finished"){
+        if (progressReturn == "Pendent" || progressReturn == "Finished") {
             return true
-        } 
+        }
         return false
+    }
+
+    const searchPoll = (searchValue) => {
+        setSearchInput(searchValue)
+        if (searchInput !== '') {
+            const dataReturn = pollList.filter((item) => {
+                return Object.values(item).join('').toLowerCase()
+                    .includes(searchInput.toLowerCase())
+            })
+            setFilter(dataReturn)
+        } else {
+            setFilter(pollList)
+        }
     }
 
     return (
@@ -70,44 +85,79 @@ export default function Home() {
                                 </li>
                             </ul>
                             <form className="d-flex">
-                                <input className="form-control me-2" type="search" placeholder="Search a Poll" aria-label="Search" />
+                                <input id="searchInput" className="form-control me-2" type="search" placeholder="Search a Poll..." aria-label="Search" onChange={(e) => searchPoll(e.target.value)} />
                             </form>
                         </div>
                     </div>
                 </nav>
             </header>
             <br /><br />
-            <table className="table table-striped w-75 text-center border border-1 m-auto">
-                <caption>Registered Polls</caption>
-                <thead className="table-dark">
-                    <tr>
-                        <th scope="col">#</th>
-                        <th scope="col">Title</th>
-                        <th scope="col">Start Date</th>
-                        <th scope="col">Termination Date</th>
-                        <th scope="col">Status</th>
-                        <th scope="col"></th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {pollList.map((val) => {
-                        return (
-                            <tr key={val.id}>
-                                <td>{val.id}</td>
-                                <td>{val.title}</td>
-                                <td>{dateFormat(val.start_date)}</td>
-                                <td>{dateFormat(val.termination_date)}</td>
-                                <td><div id="progressPoll">{progress(val.start_date, val.termination_date)}</div></td>
-                                <td><button id="voteBtn" type="button" className="vote-btn btn btn-warning rounded-pill me-3" disabled={voteController(progress(val.start_date, val.termination_date))} onClick={() => vote(val.id)}><i className="fa-solid fa-hand-point-up me-1"></i>
-                                    Vote</button>
-                                    <button id="seeBtn" type="button" className="see-btn btn btn-primary rounded-pill" onClick={() => see(val.id)}><i className="fa-solid fa-eye me-1"></i>
-                                        See</button>
-                                </td>
-                            </tr>
-                        )
-                    })}
-                </tbody>
-            </table>
+            {(searchInput.length > 0) ? (
+                <table className="table table-striped w-75 text-center border border-1 m-auto">
+                    <caption>Registered Polls</caption>
+                    <thead className="table-dark">
+                        <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">Title</th>
+                            <th scope="col">Start Date</th>
+                            <th scope="col">Termination Date</th>
+                            <th scope="col">Status</th>
+                            <th scope="col"></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {filter.map((val) => {
+                            return (
+                                <tr key={val.id}>
+                                    <td>{val.id}</td>
+                                    <td>{val.title}</td>
+                                    <td>{dateFormat(val.start_date)}</td>
+                                    <td>{dateFormat(val.termination_date)}</td>
+                                    <td><div id="progressPoll">{progress(val.start_date, val.termination_date)}</div></td>
+                                    <td><button id="voteBtn" type="button" className="vote-btn btn btn-warning rounded-pill me-3" disabled={voteController(progress(val.start_date, val.termination_date))} onClick={() => vote(val.id)}><i className="fa-solid fa-hand-point-up me-1"></i>
+                                        Vote</button>
+                                        <button id="seeBtn" type="button" className="see-btn btn btn-primary rounded-pill" onClick={() => see(val.id)}><i className="fa-solid fa-eye me-1"></i>
+                                            See</button>
+                                    </td>
+                                </tr>
+                            )
+                        })}
+                    </tbody>
+                </table>
+            ) : (
+                <table className="table table-striped w-75 text-center border border-1 m-auto">
+                    <caption>Registered Polls</caption>
+                    <thead className="table-dark">
+                        <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">Title</th>
+                            <th scope="col">Start Date</th>
+                            <th scope="col">Termination Date</th>
+                            <th scope="col">Status</th>
+                            <th scope="col"></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {filter.map((val) => {
+                            return (
+                                <tr key={val.id}>
+                                    <td>{val.id}</td>
+                                    <td>{val.title}</td>
+                                    <td>{dateFormat(val.start_date)}</td>
+                                    <td>{dateFormat(val.termination_date)}</td>
+                                    <td><div id="progressPoll">{progress(val.start_date, val.termination_date)}</div></td>
+                                    <td><button id="voteBtn" type="button" className="vote-btn btn btn-warning rounded-pill me-3" disabled={voteController(progress(val.start_date, val.termination_date))} onClick={() => vote(val.id)}><i className="fa-solid fa-hand-point-up me-1"></i>
+                                        Vote</button>
+                                        <button id="seeBtn" type="button" className="see-btn btn btn-primary rounded-pill" onClick={() => see(val.id)}><i className="fa-solid fa-eye me-1"></i>
+                                            See</button>
+                                    </td>
+                                </tr>
+                            )
+                        })}
+                    </tbody>
+                </table>
+            )}
+
         </div>
     )
 } 
